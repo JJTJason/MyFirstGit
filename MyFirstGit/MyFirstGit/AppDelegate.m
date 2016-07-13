e97ff523c522e8d1e21ce6e9e02237c9e64e753b
jiangjingtao <jiangjingtao@jd.com>
2016-07-13 13:31:47 +0800

    设置frame

--- MyFirstGit/MyFirstGit/AppDelegate.m
+++ MyFirstGit/MyFirstGit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     UIView *viee = [[UIView alloc] init];
+    viee.frame = CGRectMake(100, 100, 200, 300);
     // Override point for customization after application launch.
     return YES;
 }
